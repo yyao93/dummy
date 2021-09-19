0b854cc61d0b6c30f10b632bd3d45b3b9e43218b
Yian Yao <yianyao@Yians-Mac-mini.local>
2021-09-19 13:40:47 -0700

    add some new things

--- test.cpp
+++ test.cpp
@@ -4,11 +4,15 @@ int mymax(int a, int b) {
   return (a > b) ? a : b;
 }
 
+int mymin(int a, int b) {
+  return (a < b) ? a : b;
+}
+
 int main() {
   int n = 2;
   for (auto i = 0; i < n; i++) {
-    std::cout << i << std::endl;
+    std::cout << i;
   }
-  std::cout << mymax(3, 5) << std::endl;
+  std::cout << mymax(3, 5) << mymin(3, 5) << std::endl;
   return 0;
 }

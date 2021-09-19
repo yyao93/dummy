043650bdf460c5dc99d5d833e4aea9e2099f6010
yyao93 <yianyao1994@gmail.com>
2021-09-19 13:36:45 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,12 +3,11 @@
 int mymax(int a, int b) {
   return (a > b) ? a : b;
 }
-
 int main() {
   int n = 2;
   for (auto i = 0; i < n; i++) {
     std::cout << i << std::endl;
   }
-  std::cout << mymax(3, 5) << std::endl;
+  std::cout << mymax(30, 50) << std::endl;
   return 0;
 }
